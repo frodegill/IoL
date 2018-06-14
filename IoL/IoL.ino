4a2e7b9faba21cf0a3d96666ad015a06e2aff76f
Frode Roxrud Gill <frodegill@gmail.com>
2018-06-14 08:03:51 +0200

    Replaced with empty sensor framework

--- IoL/IoL.ino
+++ IoL/IoL.ino
@@ -7,123 +7,236 @@
  * Running on a WeMos D1 WiFi board.
  */
 
-#include <DHT.h>
-#include <TM1637Display.h>
-#include <WiFi.h>
+#include <DNSServer.h>
+#include <EEPROM.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <Ticker.h>
+
+static const char* SETUP_SSID = "sensor-setup";
+static const byte EEPROM_INITIALIZED_MARKER = 0xF1; //Just a magic number
 
 
-static const char ssid[] = "gill-roxrud";
-static const char pass[] = "******";
-WiFiServer server(80);
-
-// 7-segment TM1637
-static const byte TM1637_TEMP     = 0;
-static const byte TM1637_HUMIDITY = 1;
-static const byte TM1637_WIND     = 2;
-static const byte SEGMENTS_DIO_PIN[] = {D2,D4,D6};
-static const byte SEGMENTS_CLK_PIN[] = {D3,D5,D7};
-static const byte SEGMENTS_BRIGHTNESS = 1; // 0-7
-TM1637Display segment_displays[] = {TM1637Display(SEGMENTS_CLK_PIN[TM1637_TEMP], SEGMENTS_DIO_PIN[TM1637_TEMP]),
-                                    TM1637Display(SEGMENTS_CLK_PIN[TM1637_HUMIDITY], SEGMENTS_DIO_PIN[TM1637_HUMIDITY]),
-                                    TM1637Display(SEGMENTS_CLK_PIN[TM1637_WIND], SEGMENTS_DIO_PIN[TM1637_WIND])};
-
-// Temperature/Humidity AM2302 DHT22
-static const byte AM2302_PIN      = D8;
-#define DHTTYPE (DHT22)
-DHT dht(AM2302_PIN, DHTTYPE);
-
-// Pressure MPXV7002DP
-static const byte MPXV7002DP_PIN  = A0;
-
-unsigned long previous_update_time = 0L;
-static const int UPDATE_INTERVAL = 5000; //Should be >2000 because of DHT22
+#define SETUP_MODE_PIN              (D7)
 
 
-void readPressure(double& pressure) {
-  static const int READ_COUNT = 10;
-  double value = 0;
-  for (int i=0; i<READ_COUNT; i++)
+enum State {
+  SETUP_MODE,
+};
+
+
+ESP8266WebServer server(80);
+Ticker ticker;
+
+DNSServer dnsServer;
+static const byte DNS_PORT = 53;
+IPAddress apIP(192, 168, 4, 1);
+
+volatile State state;
+
+#define MAX_SSID_LENGTH        (32)
+#define MAX_PASSWORD_LENGTH    (64)
+
+char ssid_param[MAX_SSID_LENGTH+1];
+char password_param[MAX_PASSWORD_LENGTH+1];
+
+
+
+void onTick()
+{
+  switch(state)
   {
-    value += analogRead(MPXV7002DP_PIN); // 0-1023
+    default: break;
   }
-  value /= READ_COUNT;
-
-  pressure = -2.5 + (5.0 * value/1023.0);
 }
 
-void readTemperatureHumidity(double& temperature, double& humidity)
+void read_persistent_string(char* s, int max_length, int& adr)
 {
-  temperature = dht.readTemperature(); //Celsius
-  humidity = dht.readHumidity();
+  int i = 0;
+  byte c;
+  do {
+    c = EEPROM.read(adr++);
+    if (i<max_length)
+    {
+      s[i++] = static_cast<char>(c);
+    }
+  } while (c!=0);
+  s[i] = 0;
 }
 
-void setSegments(TM1637Display& segment_display, byte b1, boolean decimal, byte b2)
+void read_persistent_float(float& f, int decimals, int& adr)
 {
-  segment_display.showNumberDecEx(b1, decimal?0x30:0x00, false, 2, 0);
-  segment_display.showNumberDec(b2, true, 2, 2);
+  int i = 0;
+  byte c;
+  do {
+    c = EEPROM.read(adr++);
+    if (c>='0' && c<='9')
+    {
+      i = 10*i + (c-'0');
+    }
+  } while (c!=0);
+
+  f = i;
+  for (i=0; i<decimals; i++)
+  {
+    f = f/10.0f;
+  }
 }
 
-void displayDouble(TM1637Display& segment_display, double value)
+void read_persistent_params()
 {
-  int n = value;
-  int d = (value-n)*100;
-  setSegments(segment_display, n%100, true, d%100);
+  int adr = 0;
+  if (EEPROM_INITIALIZED_MARKER != EEPROM.read(adr++))
+  {
+    ssid_param[0] = 0;
+    password_param[0] = 0;
+  } else {
+    read_persistent_string(ssid_param, MAX_SSID_LENGTH, adr);
+    read_persistent_string(password_param, MAX_PASSWORD_LENGTH, adr);
+  }
+}
+
+void write_persistent_string(const char* s, size_t max_length, int& adr)
+{
+  for (int i=0; i<min(strlen(s), max_length); i++)
+  {
+    EEPROM.write(adr++, s[i]);
+  }
+  EEPROM.write(adr++, 0);
+}
+
+void write_persistent_params(const char* ssid, const char* password)
+{
+  int adr = 0;
+  EEPROM.write(adr++, EEPROM_INITIALIZED_MARKER);
+  write_persistent_string(ssid, MAX_SSID_LENGTH, adr);
+  write_persistent_string(password, MAX_PASSWORD_LENGTH, adr);
+  EEPROM.commit();
+}
+
+void handleCountConfig() {
+  server.send(200, F("text/plain"), F("graph_title InternetOfLåve\n"\
+                                      "graph_args --base 1000 -l 0\n"\
+                                      "graph_vlabel Count\n"\
+                                      "graph_category homeautomation\n"));
+}
+
+void handleCountValues() {
+  char msg[20];
+  snprintf(msg, sizeof(msg)/sizeof(msg[0]), "\n");
+  server.send(200, F("text/plain"), msg);
+}
+
+void handleSensorsConfig() {
+  server.send(200, F("text/plain"), F("graph_title InternetOfLåve\n"\
+                                      "graph_args --base 1000 -l 0\n"\
+                                      "graph_vlabel Value\n"\
+                                      "graph_category homeautomation\n"\
+                                      "graph_info IoL sensor values.\n"));
+}
+
+void handleSensorsValues() {
+  char msg[80];
+  snprintf(msg, sizeof(msg)/sizeof(msg[0]), "\n");
+  server.send(200, F("text/plain"), msg);
+}
+
+void handleNotFound() {
+  server.send(404, F("text/plain"), F("Page Not Found\n"));
+}
+
+void handleSetupRoot() {
+  if (server.hasArg("ssid") || server.hasArg("password"))
+  {
+    if (server.hasArg("ssid"))
+    {
+      strncpy(ssid_param, server.arg("ssid").c_str(), MAX_SSID_LENGTH);
+      ssid_param[MAX_SSID_LENGTH] = 0;
+    }
+    
+    if (server.hasArg("password") && !server.arg("password").equals(F("password")))
+    {
+      strncpy(password_param, server.arg("password").c_str(), MAX_PASSWORD_LENGTH);
+      password_param[MAX_PASSWORD_LENGTH] = 0;
+    }
+    
+    write_persistent_params(ssid_param, password_param);
+
+    server.send(200, F("text/plain"), F("Settings saved"));
+  }
+  else
+  {
+    read_persistent_params();
+
+    String body = F("<!doctype html>"\
+                    "<html lang=\"en\">"\
+                    "<head>"\
+                     "<meta charset=\"utf-8\">"\
+                     "<title>Setup</title>"\
+                     "<style>"\
+                      "form {margin: 0.5em;}"\
+                      "input {margin: 0.2em;}"\
+                     "</style>"\
+                    "</head>"\
+                    "<body>"\
+                     "<form method=\"post\">"\
+                      "SSID:<input type=\"text\" name=\"ssid\" required maxlength=\"");
+    body += String(MAX_SSID_LENGTH);
+    body += F("\" autofocus value=\"");
+    body += ssid_param;
+    body += F("\"/><br/>"\
+              "Password:<input type=\"password\" name=\"password\" maxlength=\"");
+    body += String(MAX_PASSWORD_LENGTH);
+    body += F("\" value=\"password\"/><br/>"\
+              "<input type=\"submit\" value=\"Submit\"/>"\
+              "</form>"\
+             "</body>"\
+             "</html>");
+    server.send(200, F("text/html"), body);
+  }
 }
 
 void setup()
 {
-  dht.begin();
+  EEPROM.begin(1 + MAX_SSID_LENGTH + 1 + MAX_PASSWORD_LENGTH + 1);
 
-  for (byte segment=TM1637_TEMP; segment<=TM1637_WIND; segment++)
+  pinMode(SETUP_MODE_PIN, INPUT_PULLUP);
+
+  if (LOW == digitalRead(SETUP_MODE_PIN))
   {
-    segment_displays[segment].setBrightness(SEGMENTS_BRIGHTNESS);
-  }
+    state = SETUP_MODE;
+    WiFi.softAP(SETUP_SSID);
+    dnsServer.start(DNS_PORT, "*", apIP);
 
-  WiFi.begin(const_cast<char*>(ssid), pass);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(100);
+    server.on("/", handleSetupRoot);
+    server.begin();
   }
-  server.begin();
+  else
+  {
+    read_persistent_params();
+    
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid_param, password_param);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+    }
+  
+    server.on(F("/count/config"), handleCountConfig);
+    server.on(F("/count/values"), handleCountValues);
+    server.on(F("/sensors/config"), handleSensorsConfig);
+    server.on(F("/sensors/values"), handleSensorsValues);
+    server.onNotFound(handleNotFound);
+    server.begin();
+  
+    onTick();
+  }
 }
 
 void loop()
 {
-  WiFiClient client = server.available();
-
-  unsigned long now = millis();
-  if (client ||
-      now<previous_update_time || //millis() wraps after ~50days
-      (now-previous_update_time)>=UPDATE_INTERVAL)
-  {
-    double temperature;
-    double humidity;
-    readTemperatureHumidity(temperature, humidity);
-  
-    double pressure;
-    readPressure(pressure);
-  
-    displayDouble(segment_displays[TM1637_TEMP], temperature);
-    displayDouble(segment_displays[TM1637_HUMIDITY], humidity);
-    displayDouble(segment_displays[TM1637_WIND], pressure);
-
-    if (client)
-    {
-      if (client.available())
-      {
-        client.print("temperature: ");
-        client.println(temperature);
-        client.print("humidity: ");
-        client.println(humidity);
-        client.print("pressure: ");
-        client.println(pressure);
-        delay(10);
-      }
-      client.stop();
-    }
-
-    previous_update_time = now;
+  if (state == SETUP_MODE) {
+      dnsServer.processNextRequest();
   }
-  
-  delay(25);
-}
 
+  server.handleClient();
+}
