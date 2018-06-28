1ecfbdc5ee19a207467d9d94d1309e546e151b30
Frode Roxrud Gill <frodegill@gmail.com>
2018-06-28 17:53:11 +0200

    Actual functionality..

--- IoL/IoL.ino
+++ IoL/IoL.ino
@@ -7,23 +7,55 @@
  * Running on a WeMos D1 WiFi board.
  */
 
+#include <DHTesp.h>
 #include <DNSServer.h>
 #include <EEPROM.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include "Sensirion.h"
 #include <Ticker.h>
 
 static const char* SETUP_SSID = "sensor-setup";
 static const byte EEPROM_INITIALIZED_MARKER = 0xF1; //Just a magic number
 
+static const uint8_t DHT_TEMP_HUMIDITY_SENSOR        = 1<<0;
+static const uint8_t PRECISION_TEMP_HUMIDITY_SENSOR1 = 1<<1;
+static const uint8_t PRECISION_TEMP_HUMIDITY_SENSOR2 = 1<<2;
+static const uint8_t PRESSURE_DIFF_SENSOR            = 1<<3;
+static const uint8_t WINDSPEED_SENSOR                = 1<<4;
+static const uint8_t FAN_RELAY                       = 1<<5;
+static const uint8_t THERMOSTAT_PLUG                 = 1<<6;
 
-#define SETUP_MODE_PIN              (D7)
+
+#define PRESSURE_DIFF_PIN            (A0) //Max 3.3V
+//#define (TX)
+//#define (RX)
+//#define (D0) //Neither Interrupt, PWM, I2C nor One-wire
+#define DHT_PIN                      (D1) //HW I2C
+#define PRECISION_TEMP1_DATA_PIN     (D2) //HW I2C
+#define PRECISION_TEMP2_DATA_PIN     (D3) //10K pull-up
+#define SETUP_MODE_PIN               (D4) //10K pull-up, built-in LED
+#define PRECISION_TEMP_SYNC_PIN      (D5)
+#define WINDSPEED_PIN                (D6)
+#define FAN_RELAY_TRIGGER_PIN        (D7)
+#define THERMOSTAT_PLUG_TRIGGER_PIN  (D8) //10K pull-down
+
+#define DELAY_BETWEEN_ACTIVE_SENSORS (25) //ms between reading different sensors
+#define SENSOR_READ_DELAY_TIME       (10*1000)  //ms min. time between sensor reading cycle
 
 
 enum State {
   SETUP_MODE,
+  READ_TEMP_HUMIDITY_SENSOR,
+  READ_PRECISION_TEMP1_SENSOR,
+  READ_PRECISION_TEMP2_SENSOR,
+  READ_PRESSURE_DIFF_SENSOR
 };
 
+
+DHTesp dht;
+Sensirion sht1 = Sensirion(PRECISION_TEMP1_DATA_PIN, PRECISION_TEMP_SYNC_PIN, 0x40);
+Sensirion sht2 = Sensirion(PRECISION_TEMP2_DATA_PIN, PRECISION_TEMP_SYNC_PIN, 0x40);
 
 ESP8266WebServer server(80);
 Ticker ticker;
@@ -39,6 +71,15 @@ volatile State state;
 
 char ssid_param[MAX_SSID_LENGTH+1];
 char password_param[MAX_PASSWORD_LENGTH+1];
+uint8_t active_sensors_param = 0;
+
+volatile bool should_read_dht_temp_sensor;
+float dht_tempsensor_value;
+float dht_humiditysensor_value;
+
+volatile bool should_read_sht_temp_sensor[2];
+float sht_tempsensor_value[2];
+float sht_humiditysensor_value[2];
 
 
 
@@ -46,6 +87,50 @@ void onTick()
 {
   switch(state)
   {
+    case READ_TEMP_HUMIDITY_SENSOR:
+      {
+        if (active_sensors_param & DHT_TEMP_HUMIDITY_SENSOR) {
+          should_read_dht_temp_sensor = true;
+        }
+
+        state = READ_PRECISION_TEMP1_SENSOR;
+        ticker.attach_ms(DELAY_BETWEEN_ACTIVE_SENSORS, onTick);
+        break;  
+      }
+
+    case READ_PRECISION_TEMP1_SENSOR:
+      {
+        if (active_sensors_param & PRECISION_TEMP_HUMIDITY_SENSOR1) {
+          should_read_sht_temp_sensor[0] = true;
+        }
+        
+        state = READ_PRECISION_TEMP2_SENSOR;
+        ticker.attach_ms(DELAY_BETWEEN_ACTIVE_SENSORS, onTick);
+        break;
+      }
+    
+    case READ_PRECISION_TEMP2_SENSOR:
+      {
+        if (active_sensors_param & PRECISION_TEMP_HUMIDITY_SENSOR2) {
+          should_read_sht_temp_sensor[1] = true;
+        }
+
+        state = READ_PRESSURE_DIFF_SENSOR;
+        ticker.attach_ms(DELAY_BETWEEN_ACTIVE_SENSORS, onTick);
+        break;
+      }
+    
+    case READ_PRESSURE_DIFF_SENSOR:
+      {
+        if (active_sensors_param & READ_PRESSURE_DIFF_SENSOR) {
+          int value = max(0, min(1023, analogRead(PRESSURE_DIFF_PIN)));
+        }
+        
+        state = READ_PRECISION_TEMP1_SENSOR;
+        ticker.attach_ms(SENSOR_READ_DELAY_TIME, onTick);
+        break;
+      }
+    
     default: break;
   }
 }
@@ -64,23 +149,9 @@ void read_persistent_string(char* s, int max_length, int& adr)
   s[i] = 0;
 }
 
-void read_persistent_float(float& f, int decimals, int& adr)
+void read_persistent_byte(uint8_t& b, int& adr)
 {
-  int i = 0;
-  byte c;
-  do {
-    c = EEPROM.read(adr++);
-    if (c>='0' && c<='9')
-    {
-      i = 10*i + (c-'0');
-    }
-  } while (c!=0);
-
-  f = i;
-  for (i=0; i<decimals; i++)
-  {
-    f = f/10.0f;
-  }
+  b = EEPROM.read(adr++);
 }
 
 void read_persistent_params()
@@ -90,9 +161,11 @@ void read_persistent_params()
   {
     ssid_param[0] = 0;
     password_param[0] = 0;
+    active_sensors_param = 0;
   } else {
     read_persistent_string(ssid_param, MAX_SSID_LENGTH, adr);
     read_persistent_string(password_param, MAX_PASSWORD_LENGTH, adr);
+    read_persistent_byte(active_sensors_param, adr);
   }
 }
 
@@ -105,12 +178,18 @@ void write_persistent_string(const char* s, size_t max_length, int& adr)
   EEPROM.write(adr++, 0);
 }
 
-void write_persistent_params(const char* ssid, const char* password)
+void write_persistent_byte(uint8_t b, int& adr)
+{
+  EEPROM.write(adr++, b);
+}
+
+void write_persistent_params(const char* ssid, const char* password, uint8_t active_sensors)
 {
   int adr = 0;
   EEPROM.write(adr++, EEPROM_INITIALIZED_MARKER);
   write_persistent_string(ssid, MAX_SSID_LENGTH, adr);
   write_persistent_string(password, MAX_PASSWORD_LENGTH, adr);
+  write_persistent_byte(active_sensors, adr);
   EEPROM.commit();
 }
 
@@ -159,8 +238,18 @@ void handleSetupRoot() {
       strncpy(password_param, server.arg("password").c_str(), MAX_PASSWORD_LENGTH);
       password_param[MAX_PASSWORD_LENGTH] = 0;
     }
-    
-    write_persistent_params(ssid_param, password_param);
+
+    active_sensors_param = 0;
+    if (server.hasArg("sensor0")) {active_sensors_param |= 1<<0;}
+    if (server.hasArg("sensor1")) {active_sensors_param |= 1<<1;}
+    if (server.hasArg("sensor2")) {active_sensors_param |= 1<<2;}
+    if (server.hasArg("sensor3")) {active_sensors_param |= 1<<3;}
+    if (server.hasArg("sensor4")) {active_sensors_param |= 1<<4;}
+    if (server.hasArg("sensor5")) {active_sensors_param |= 1<<5;}
+    if (server.hasArg("sensor6")) {active_sensors_param |= 1<<6;}
+    if (server.hasArg("sensor7")) {active_sensors_param |= 1<<7;}
+
+    write_persistent_params(ssid_param, password_param, active_sensors_param);
 
     server.send(200, F("text/plain"), F("Settings saved"));
   }
@@ -188,6 +277,35 @@ void handleSetupRoot() {
               "Password:<input type=\"password\" name=\"password\" maxlength=\"");
     body += String(MAX_PASSWORD_LENGTH);
     body += F("\" value=\"password\"/><br/>"\
+                      "Internal temp/humidity sensor: <input type=\"checkbox\" name=\"sensor0\"");
+    if (active_sensors_param&DHT_TEMP_HUMIDITY_SENSOR)
+      body += F(" checked");
+
+    body += F("/><br/>External temp sensor 1: <input type=\"checkbox\" name=\"sensor1\"");
+    if (active_sensors_param&PRECISION_TEMP_HUMIDITY_SENSOR1)
+      body += F(" checked");
+    
+    body += F("/><br/>External temp sensor 2: <input type=\"checkbox\" name=\"sensor2\"");
+    if (active_sensors_param&PRECISION_TEMP_HUMIDITY_SENSOR2)
+      body += F(" checked");
+    
+    body += F("/><br/>Pressure diff. sensor: <input type=\"checkbox\" name=\"sensor3\"");
+    if (active_sensors_param&PRESSURE_DIFF_SENSOR)
+      body += F(" checked");
+    
+    body += F("/><br/>Windspeed sensor: <input type=\"checkbox\" name=\"sensor4\"");
+    if (active_sensors_param&WINDSPEED_SENSOR)
+      body += F(" checked");
+    
+    body += F("/><br/>Fan relay: <input type=\"checkbox\" name=\"sensor5\"");
+    if (active_sensors_param&FAN_RELAY)
+      body += F(" checked");
+    
+    body += F("/><br/>Heater thermostat plug: <input type=\"checkbox\" name=\"sensor6\"");
+    if (active_sensors_param&THERMOSTAT_PLUG)
+      body += F(" checked");
+    
+    body += F("/><br/>"\
               "<input type=\"submit\" value=\"Submit\"/>"\
               "</form>"\
              "</body>"\
@@ -199,6 +317,8 @@ void handleSetupRoot() {
 void setup()
 {
   EEPROM.begin(1 + MAX_SSID_LENGTH + 1 + MAX_PASSWORD_LENGTH + 1);
+
+  dht.setup(DHT_PIN, DHTesp::AUTO_DETECT);
 
   pinMode(SETUP_MODE_PIN, INPUT_PULLUP);
 
@@ -228,6 +348,14 @@ void setup()
     server.onNotFound(handleNotFound);
     server.begin();
   
+    should_read_dht_temp_sensor = false;
+    dht_tempsensor_value = dht_humiditysensor_value = 0.0f;
+
+    should_read_sht_temp_sensor[0] = should_read_sht_temp_sensor[1] = false;
+    sht_tempsensor_value[0] = sht_tempsensor_value[1] = 0.0f;
+    sht_humiditysensor_value[0] = sht_humiditysensor_value[1] = 0.0f;
+
+    state = READ_TEMP_HUMIDITY_SENSOR;
     onTick();
   }
 }
@@ -239,4 +367,33 @@ void loop()
   }
 
   server.handleClient();
+
+  if ((active_sensors_param & DHT_TEMP_HUMIDITY_SENSOR) && should_read_dht_temp_sensor) {
+    should_read_dht_temp_sensor = false;
+    delay(dht.getMinimumSamplingPeriod());
+    TempAndHumidity temp_and_humidity = dht.getTempAndHumidity();
+    if (dht.getStatus() == DHTesp::ERROR_NONE)
+    {
+      dht_tempsensor_value = temp_and_humidity.temperature;
+      dht_humiditysensor_value = temp_and_humidity.humidity;
+    }
+  }
+
+  if ((active_sensors_param & PRECISION_TEMP_HUMIDITY_SENSOR1) && should_read_sht_temp_sensor[0]) {
+    float tmp_temp, tmp_humidity;
+    if (S_Meas_Rdy == sht1.measure(&tmp_temp, &tmp_humidity)) {
+      should_read_sht_temp_sensor[0] = false;
+      sht_tempsensor_value[0] = tmp_temp;
+      sht_humiditysensor_value[0] = tmp_humidity;
+    }
+  }
+
+  if ((active_sensors_param & PRECISION_TEMP_HUMIDITY_SENSOR2) && should_read_sht_temp_sensor[1]) {
+    float tmp_temp, tmp_humidity;
+    if (S_Meas_Rdy == sht2.measure(&tmp_temp, &tmp_humidity)) {
+      should_read_sht_temp_sensor[1] = false;
+      sht_tempsensor_value[1] = tmp_temp;
+      sht_humiditysensor_value[1] = tmp_humidity;
+    }
+  }
 }
